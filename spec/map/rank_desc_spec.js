244a31cb6acb39d9f720ba1b3866e66149a77923
Zhu Mei <meizhu812@outlook.com>
2019-12-26 10:44:21 +0800

    Update rank_desc_spec.js

--- spec/map/rank_desc_spec.js
+++ spec/map/rank_desc_spec.js
@@ -3,9 +3,9 @@ var rank_desc = require('../../main/map/rank_desc.js');
 
 describe('rank by desc', function() {
   var collection_a = [3, 2, 4, 5, 6];
-  var collection_b = [2, 3, 4, 5, 6];
+  var collection_b = [6, 5, 4, 3, 2];
 
-  it('从小到大排序', function() {
+  it('从大到小排序', function() {
     var result = rank_desc(collection_a);
     expect(result).toEqual(collection_b);
   });
